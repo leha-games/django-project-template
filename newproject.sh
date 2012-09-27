2e308fe3878acb4748a28fd9b069ed562fdc3db2
Alexey Grigoriev <grialexey@gmail.com>
2012-09-27 10:36:49 +0400

    added shell script to create new project, added db, static, media folders

---
+++ newproject.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+source ~/.profile
+cd ~
+cd sites
+mkvirtualenv --no-site-packages django-$1-env
+pip install Django
+django-admin.py startproject --template https://bitbucket.org/grialexey/django-project-template/get/master.zip $1
+cd $1
+pip install -r requirements.txt
+cp $1/settings/local-dev.py $1/settings/local.py
+python manage.py syncdb
+python manage.py migrate
+python manage.py runserver
+git init
