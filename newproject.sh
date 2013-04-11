7feda94fe5adb27fcb98f35fffe6c0164c5b9bc4
Alexey Grigoriev <grialexey@gmail.com>
2013-04-11 10:00:19 +0400

    rename myutils to common

--- newproject.sh
+++ newproject.sh
@@ -7,6 +7,7 @@ pip install Django
 django-admin.py startproject --template https://bitbucket.org/grialexey/django-project-template/get/master.zip $1
 cd $1
 pip install -r requirements.txt
+pip freeze > requirements.txt
 cp $1/settings/local-dev.py $1/settings/local.py
 python manage.py syncdb
 python manage.py migrate
