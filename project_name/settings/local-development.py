7bda516ca685177539f1dcb29de364829cc77853
Alexey Grigoriev <grialexey@gmail.com>
2013-11-20 18:10:38 +0400

    fix. database in dev environment now gets created in project root db/ dir

--- project_name/settings/local-development.py
+++ project_name/settings/local-development.py
@@ -19,7 +19,7 @@ TEMPLATE_DEBUG = True
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),                      # Or path to database file if using sqlite3.
+        'NAME': os.path.join(BASE_DIR, '../db', 'db.sqlite3'),                      # Or path to database file if using sqlite3.
     }
 }
 
