aa369f9ad1ddd950ce28f0aa953c39a02f251d7a
Alexey Grigoriev <grialexey@gmail.com>
2013-11-22 12:24:44 +0400

    set STATIC_ROOT and MEDIA_ROOT production settings pointing to /var/www/project_name/(static,media) directories

--- project_name/settings/local-production.py
+++ project_name/settings/local-production.py
@@ -38,7 +38,7 @@ DATABASES = {
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = '/var/www/{{ project_name }}/media/'
 
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
@@ -51,7 +51,7 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = '/var/www/{{ project_name }}/static/'
 
 
 # URL prefix for static files.
