89de884976eabb5d473d071d0022c5d8471127e9
Alexey Grigoriev <grialexey@gmail.com>
2013-11-27 17:54:36 +0400

    Home view now uses Class Based Generic View TemplateView

--- project_name/apps/base/urls.py
+++ project_name/apps/base/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url, patterns
+from django.views.generic.base import TemplateView
 
-
-urlpatterns = patterns('{{ project_name}}.apps.base.views',
-    url(r'^$', 'home', name='home'),
+urlpatterns = patterns('',
+    url(r'^$', TemplateView.as_view(template_name="base/home.html"), name='home'),
 )
--- project_name/apps/base/views.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from django.shortcuts import render_to_response
-from django.template import RequestContext
-
-
-def home(request):
-    """ Default view for the root """
-    return render_to_response('base/home.html',
-        context_instance=RequestContext(request))
