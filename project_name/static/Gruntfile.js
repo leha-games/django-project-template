b350b2e83a8ebeb7dce15a0be3e5ded1e3e4dc06
Alexey Grigoriev <grialexey@gmail.com>
2013-12-22 21:05:45 +0400

    Bug with wrong stylus files folder in gruntfile

--- project_name/static/Gruntfile.js
+++ project_name/static/Gruntfile.js
@@ -32,7 +32,7 @@ module.exports = function(grunt) {
             }
         },
         watch: {
-            files: ['css/*'],
+            files: ['styl/*'],
             tasks: ['stylus']
         }
     });
