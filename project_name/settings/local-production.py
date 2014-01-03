4adb8fc5a3fa520cb1c55bfd57a644267de6e4e4
Alexey Grigoriev <grialexey@gmail.com>
2014-01-03 20:50:59 +0400

    Typo in production settings file

--- project_name/settings/local-production.py
+++ project_name/settings/local-production.py
@@ -21,7 +21,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': '{{ project_name }}',                        # Or path to database file if using sqlite3.
-        'USER': os.environ['DATABSE_USER'],                  # Not used with sqlite3.
+        'USER': os.environ['DATABASE_USER'],                  # Not used with sqlite3.
         'PASSWORD': os.environ['DATABASE_PASSWORD'],         # Not used with sqlite3.
         'HOST': '',                                          # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                                          # Set to empty string for default. Not used with sqlite3.
