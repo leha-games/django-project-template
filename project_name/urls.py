d51afe33049ef5b8bc1f1be013fa91ad73fe04b0
Alexey Grigoriev <grialexey@gmail.com>
2016-01-27 20:36:00 +0300

    Fixes for deployment scripts

--- project_name/urls.py
+++ project_name/urls.py
@@ -1,11 +1,11 @@
 from django.conf import settings
-from django.conf.urls import patterns, include, url
-from django.views.generic.base import TemplateView
+from django.conf.urls import include, url
 from django.conf.urls.static import static
-
+from django.views.generic.base import TemplateView
+from django.views.defaults import page_not_found, server_error
 from django.contrib import admin
-admin.autodiscover()
 
+admin.site.site_header = u'Django administration'
 
 urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name="home.html")),
@@ -15,8 +15,8 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += [
-        url(r'^404/$', 'django.views.defaults.page_not_found'),
-        url(r'^500/$', 'django.views.defaults.server_error'),
+        url(r'^404/$', page_not_found),
+        url(r'^500/$', server_error),
         url(r'^admin/', include(admin.site.urls)),
     ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
       + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
