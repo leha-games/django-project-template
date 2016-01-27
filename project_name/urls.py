89b085da5c70d90e720e34dd8b5ce13570514254
Alexey Grigoriev <grialexey@gmail.com>
2016-01-27 18:28:21 +0300

    Some fixes

--- project_name/urls.py
+++ project_name/urls.py
@@ -1,23 +1,22 @@
 from django.conf import settings
 from django.conf.urls import patterns, include, url
 from django.views.generic.base import TemplateView
+from django.conf.urls.static import static
 
 from django.contrib import admin
 admin.autodiscover()
 
 
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name="home.html")),
     url(r'^{{ project_name }}admin/', include(admin.site.urls)),
-)
+]
 
 
 if settings.DEBUG:
-    urlpatterns += patterns('',
-            (r'^%s/(?P<path>.*)$' % settings.MEDIA_URL[1:-1],
-                'django.views.static.serve',
-                {'document_root': settings.MEDIA_ROOT}),
-            (r'^404/$', 'django.views.defaults.page_not_found'),
-            (r'^500/$', 'django.views.defaults.server_error'),
-            (r'^admin/', include(admin.site.urls)),
-    )
+    urlpatterns += [
+        url(r'^404/$', 'django.views.defaults.page_not_found'),
+        url(r'^500/$', 'django.views.defaults.server_error'),
+        url(r'^admin/', include(admin.site.urls)),
+    ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
+      + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
