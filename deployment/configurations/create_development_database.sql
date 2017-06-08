41001d3ca03e95c6efdebf415623bccb59b9f039
Alexey Grigoriev <grialexey@gmail.com>
2017-06-08 15:10:40 +0300

    Change password for local development to "password"

--- deployment/configurations/create_development_database.sql
+++ deployment/configurations/create_development_database.sql
@@ -1,5 +1,5 @@
 CREATE DATABASE {{ project_name }} ENCODING 'UTF-8' LC_COLLATE 'en_US.UTF-8' LC_CTYPE 'en_US.UTF-8' TEMPLATE template0;
-CREATE USER {{ project_name }} WITH PASSWORD 'development_password';
+CREATE USER {{ project_name }} WITH PASSWORD 'password';
 ALTER ROLE {{ project_name }} SET client_encoding TO 'utf8';
 ALTER ROLE {{ project_name }} SET default_transaction_isolation TO 'read committed';
 ALTER ROLE {{ project_name }} SET timezone TO 'UTC';
